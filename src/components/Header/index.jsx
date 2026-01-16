51704dd5120e3bcbd2767be0dc920cd501949130
mirazim44 <mirobidovmirazim703@gmail.com>
2026-01-16 15:34:53 +0500

    header and section one

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,21 @@
 
 import './style.css';
 
+import logo from '../../assets/kol.png'
+
 function Header() {
     return (
-        <div>
-            <h1>Header</h1>
-        </div>
+         <header className="header cont">
+      <img src={logo}alt="" />
+
+      <nav className="nav">
+        <p>Home</p>
+        <p>Feature</p>
+        <p>About</p>
+        <p>Product</p>
+        <button className="shop-btn">Shop Now</button>
+      </nav>
+    </header>
     );
 }
 
